96f3ccb69becd5642c3da65c58a0d1da5aeacdce
sasi-github <56940306+sasi-github@users.noreply.github.com>
2019-11-18 00:51:19 +0000

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+  res.send({ express: 'Hello from Express' });
 });
 
 app.post('/api/world', (req, res) => {
